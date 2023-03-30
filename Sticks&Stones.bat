8adaadd339dbec7e071814f73b9dccfce5a6fed9
Flowzy12123 <83522870+Flowzy12123@users.noreply.github.com>
2023-03-30 18:32:04 -0400

    Add files via upload

---
+++ Sticks&Stones.bat
@@ -0,0 +1,37 @@
+@echo off & cls
+title BEAMED BY FLOWZY
+mode con cols=150 lines=100
+color p
+echo.
+:flowz                                                                                                                                                                                                                                                                                                      
+echo FFFFFFFFFFFFFFFFFFFFFFLLLLLLLLLLL                  OOOOOOOOO     WWWWWWWW                           WWWWWWWWZZZZZZZZZZZZZZZZZZZYYYYYYY       YYYYYYY
+echo F::::::::::::::::::::FL:::::::::L                OO:::::::::OO   W::::::W                           W::::::WZ:::::::::::::::::ZY:::::Y       Y:::::Y
+echo F::::::::::::::::::::FL:::::::::L              OO:::::::::::::OO W::::::W                           W::::::WZ:::::::::::::::::ZY:::::Y       Y:::::Y
+echo FF::::::FFFFFFFFF::::FLL:::::::LL             O:::::::OOO:::::::OW::::::W                           W::::::WZ:::ZZZZZZZZ:::::Z Y::::::Y     Y::::::Y
+echo  F:::::F       FFFFFF  L:::::L               O::::::O   O::::::O W:::::W           WWWWW           W:::::W ZZZZZ     Z:::::Z  YYY:::::Y   Y:::::YYY
+echo  F:::::F               L:::::L               O:::::O     O:::::O  W:::::W         W:::::W         W:::::W          Z:::::Z       Y:::::Y Y:::::Y   
+echo  F::::::FFFFFFFFFF     L:::::L               O:::::O     O:::::O   W:::::W       W:::::::W       W:::::W          Z:::::Z         Y:::::Y:::::Y    
+echo  F:::::::::::::::F     L:::::L               O:::::O     O:::::O    W:::::W     W:::::::::W     W:::::W          Z:::::Z           Y:::::::::Y     
+echo  F:::::::::::::::F     L:::::L               O:::::O     O:::::O     W:::::W   W:::::W:::::W   W:::::W          Z:::::Z             Y:::::::Y      
+echo  F::::::FFFFFFFFFF     L:::::L               O:::::O     O:::::O      W:::::W W:::::W W:::::W W:::::W          Z:::::Z               Y:::::Y       
+echo  F:::::F               L:::::L               O:::::O     O:::::O       W:::::W:::::W   W:::::W:::::W          Z:::::Z                Y:::::Y       
+echo  F:::::F               L:::::L         LLLLLLO::::::O   O::::::O        W:::::::::W     W:::::::::W        ZZZ:::::Z     ZZZZZ       Y:::::Y       
+echo FF:::::::FF           LL:::::::LLLLLLLLL:::::LO:::::::OOO:::::::O         W:::::::W       W:::::::W         Z::::::ZZZZZZZZ:::Z       Y:::::Y       
+echo F::::::::FF           L::::::::::::::::::::::L OO:::::::::::::OO           W:::::W         W:::::W          Z:::::::::::::::::Z    YYYY:::::YYYY    
+echo F::::::::FF           L::::::::::::::::::::::L   OO:::::::::OO              W:::W           W:::W           Z:::::::::::::::::Z    Y:::::::::::Y    
+echo FFFFFFFFFFF           LLLLLLLLLLLLLLLLLLLLLLLL     OOOOOOOOO                 WWW             WWW            ZZZZZZZZZZZZZZZZZZZ    YYYYYYYYYYYYY                                   
+echo.
+echo.
+echo [40;37m   Welcome back [40;36m%USERNAME%[40;37m!
+echo.
+:main
+echo BEAMED BY FLOWZY
+echo BEAMED BY FLOWZY
+echo BEAMED BY FLOWZY
+echo BEAMED BY FLOWZY
+echo BEAMED BY FLOWZY
+echo BEAMED BY FLOWZY
+echo BEAMED BY FLOWZY
+echo BEAMED BY FLOWZY
+echo BEAMED BY FLOWZY
+goto flowz
